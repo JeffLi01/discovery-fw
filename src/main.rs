18c1c1b1eb09c38f0cc924c7b56222dde7ff4d59
Jeff Li <lijinfeng01@inspur.com>
2023-06-29 15:22:19 +0800

    feat: press UserButton to change board mode
    Signed-off-by: Jeff Li <lijinfeng01@inspur.com>

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
 use core::sync::atomic::{AtomicU8, Ordering};
 
 // pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // use panic_abort as _; // requires nightly
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 
 use cortex_m::delay::Delay;
+use cortex_m::interrupt::Mutex;
 use cortex_m::Peripherals;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
+use stm32f3::stm32f303::{interrupt, Interrupt, NVIC};
 use stm32f3_discovery::leds::{Direction as LedDirection, Leds};
+use stm32f3_discovery::stm32f3xx_hal::gpio::{Edge, Gpioa, Input, Pin, U};
 use stm32f3_discovery::stm32f3xx_hal::pac;
 use stm32f3_discovery::stm32f3xx_hal::prelude::*;
 use stm32f3_discovery::switch_hal::OutputSwitch;
 
 static BOARD_MODE: AtomicU8 = AtomicU8::new(0);
+
+static PA0: Mutex<RefCell<Option<Pin<Gpioa, U<0>, Input>>>> = Mutex::new(RefCell::new(None));
 
 struct Discovery {
     delay: Delay,
@@ -31,6 +37,7 @@ impl Discovery {
         let device_periph = pac::Peripherals::take().unwrap();
 
         let mut rcc = device_periph.RCC.constrain();
+        let mut syscfg = device_periph.SYSCFG.constrain(&mut rcc.apb2);
 
         let syst = core_peripherals.SYST;
         let delay = Delay::new(syst, 8_000_000);
@@ -51,17 +58,56 @@ impl Discovery {
             &mut otyper,
         );
 
+        let mut gpioa = device_periph.GPIOA.split(&mut rcc.ahb);
+        let mut exti = device_periph.EXTI;
+        let mut pa0 = gpioa.pa0.into_input(&mut gpioa.moder);
+        pa0.trigger_on_edge(&mut exti, Edge::Rising);
+        pa0.make_interrupt_source(&mut syscfg);
+        pa0.enable_interrupt(&mut exti);
+
+        cortex_m::interrupt::free(|cs| PA0.borrow(cs).replace(Some(pa0)));
+
         Discovery { delay, leds }
     }
 
     fn mainloop(&mut self) -> ! {
+        unsafe {
+            NVIC::unmask(Interrupt::EXTI0);
+        }
         loop {
             let mode = BOARD_MODE.load(Ordering::Relaxed);
             hprintln!("Mode {}", mode).unwrap();
             match mode {
                 0 => self.breath_led(LedDirection::North),
-                1 => {}
+                1 => self.breath_leds(),
                 _ => BOARD_MODE.store(0, Ordering::Relaxed),
+            }
+        }
+    }
+
+    fn breath_leds(&mut self) {
+        let orig_mode = BOARD_MODE.load(Ordering::Relaxed);
+        let mut up = true;
+        let mut duty = 0;
+        let cycle = 1000;
+        let step = 1;
+        while BOARD_MODE.load(Ordering::Relaxed) == orig_mode {
+            for led in &mut self.leds {
+                led.on().unwrap();
+            }
+            self.delay.delay_us(duty);
+            for led in &mut self.leds {
+                led.off().unwrap();
+            }
+            self.delay.delay_us(cycle - duty);
+            match up {
+                true => duty += step,
+                false => duty -= step,
+            }
+            if duty == cycle {
+                up = false;
+            } else if duty == 0 {
+                up = true;
             }
         }
     }
@@ -95,4 +141,14 @@ fn main() -> ! {
     let mut discovery = Discovery::new();
 
     discovery.mainloop();
+}
+
+#[interrupt]
+fn EXTI0() {
+    cortex_m::interrupt::free(|cs| {
+        if let Some(pa0) = (*PA0.borrow(cs).borrow_mut()).as_mut() {
+            pa0.clear_interrupt_pending_bit();
+        }
+        BOARD_MODE.fetch_add(1, Ordering::Relaxed);
+    });
 }
